d7daba610fedcee60dadd4f14d52b8384b8184e0
SIAM0808 <fahadhasan1425@gmail.com>
2024-07-17 23:05:52 +0600

    updating files

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 const lowerChars = "qwertyuiopasdfghjklzxcvbnm"
 const upperChars = "ASDFGHJKLQWERTYUIOPMNBVCXZ"
 const numbers = "1234567890"
-const symbol = "#@!$%^)(*&_+-={}[]?/| "
+const symbol = "#@!$%^*&_+-? "
 
 
-const lch = document.querySelector("lch")
-const uppCh = document.querySelector("uppch")
-const nm = document.querySelector("nm")
-const sym = document.querySelector("sym")
-const length = Number(document.querySelector("length").value)
-const result = document.querySelector("result")
-const button = document.querySelector("button")
+const lch = document.querySelector("#lch")
+const uppCh = document.querySelector("#uppch")
+const nm = document.querySelector("#nm")
+const sym = document.querySelector("#sym")
+const lengthInput = document.querySelector("#length")
+const result = document.querySelector("#result")
+const button = document.querySelector("#button")
 
 
 
-function generator(allowed, length){
-    let alowed = "";
-alowed += lch.checked ? lowerChars : ""
-alowed += uppCh.checked ? upperChars : ""
-alowed += nm.checked ? numbers : ""
-alowed += sym.checked ? symbol : ""
+function generator(allowed, length) {
+
     let ans = ""
-    for(let i=0; i<length; i++){
+    for (let i = 0; i < length; i++) {
         let index = Math.floor(Math.random() * allowed.length)
         ans += allowed[index];
     }
@@ -29,8 +25,14 @@ alowed += sym.checked ? symbol : ""
 }
 
 
-button.onclick = function(){
-    let pass = generator(alowed, length);
+button.onclick = function () {
+    let allowed = "";
+    allowed += lch.checked ? lowerChars : ""
+    allowed += uppCh.checked ? upperChars : ""
+    allowed += nm.checked ? numbers : ""
+    allowed += sym.checked ? symbol : ""
+    const length = Number(lengthInput.value)
+    let pass = generator(allowed, length);
     result.textContent = pass;
-}
+};
 
